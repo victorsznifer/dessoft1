b61031c204b93ce6fa191c231f49cda63ea40b56
victorsznifer <victorsl@al.insper.edu.br>
2017-03-17 11:00:23 -0300

    vitao

---
+++ aluno1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 10:32:01 2017
+
+@author: victo
+"""
+
+
+def ler_numeros(n):
+    lista=[]
+    for i in range(n):
+        n = int(input("escreva um numero"))
+        lista.append(n)
+    return lista()
